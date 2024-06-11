53aad8157a8ffaf9d5ebb98f21c500f3bdfd27ac
Lens_r <lensplaysgames@gmail.com>
2024-06-11 13:19:15 -0700

    [GOAL COMPLETE] Very basic, but hey, it works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,45 @@
+function VideoPlayer({ url, title, info, date }) {
+  if (!url) return "No URL passed to VideoPlayer...";
+
+  return (
+    <div>
+      <video
+        preload="metadata"
+        controls
+        src={url}
+      >
+        Your browser does not support this video, sorry
+      </video>
+
+      <div className="flex justify-between">
+
+        { title
+          ? <h2>{title}</h2>
+          : <h2 className="text-slate-500">No Title</h2>
+        }
+
+        { date
+          ? <p>{date.toLocaleDateString()}</p>
+          : null
+        }
+
+      </div>
+
+      { info
+        ? <p className="text-gray-400">{info}</p>
+        : <p className="text-slate-500">No Info</p>
+      }
+
+    </div>
+  )
+}
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-4">
       {// Navbar
       }
-      <div className="flex justify-between align-center w-full mb-1">
+      <div className="flex justify-between align-center w-full mb-2">
         <div id="navbar-left">
           <h1>Curator</h1>
         </div>
@@ -15,7 +51,14 @@ export default function Home() {
       {// Page Content
       }
       <div>
-        <video src="">Your browser does not support this video, sorry</video>
+        {// TODO: The idea would be to eventually have this data filled in from a database by this page.
+        }
+        <VideoPlayer
+          url="https://github.com/ciaranbench/ciaranbench.github.io/raw/main/assets/dance_ex.mp4"
+          title="GitHub Dance Example"
+          info="A random dance video I found on GitHub and am using as an example of an externally hosted video streamed from this site."
+          date={new Date()}
+        />
       </div>
     </main>
   );
