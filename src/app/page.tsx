352d9d4c7620e216062acc215afce55b60dacc39
Lens_r <lensplaysgames@gmail.com>
2024-06-12 22:00:35 -0700

    Display list of user's posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
 "use client";
 
 import { SignInButton } from "./signInButton";
-import { firebaseDb } from "@/libs/firebase/config"
-import { signOut, UserContext } from "@/libs/firebase/auth"
-import { deleteDoc, doc, setDoc } from "firebase/firestore";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import {
+  arrayUnion,
+  collection,
+  deleteDoc,
+  doc,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  setDoc
+} from "firebase/firestore";
+import { firebaseDb } from "@/libs/firebase/config";
+import {
+  firebaseAuth,
+  onAuthStateChanged,
+  signOut,
+  UserContext
+} from "@/libs/firebase/auth";
+import {
+  HOSTNAME
+} from "@/constants";
 
 export default function Home() {
   const {user, setUser} = useContext(UserContext);
@@ -13,118 +32,212 @@ export default function Home() {
   const [inputPostInfo, setInputPostInfo] = useState<string>("");
   const [inputPostContentURI, setInputPostContentURI] = useState<string>("");
   const [inputPostThumbnailURI, setInputPostThumbnailURI] = useState<string>("");
+  const [posts, setPosts] = useState<Array<any>>([]);
+
+  async function postPost(posterUserId: string, title: string, info: string, contentURI: string, thumbnailURI: string) {
+    // Generate a universally unique post ID; we don't check for collisions
+    // because the chances of that are less than multiple bit flips happening
+    // randomly and causing an error in the program.
+    const postId = uuidv4();
+
+    const post = {
+      title,
+      info,
+      contentURI,
+      thumbnailURI,
+      date: new Date(),
+      type: "video",
+    };
+
+    setPosts(oldPosts => {
+      const newPosts = oldPosts.concat({ id: postId, ...post });
+      // Sort by date, most recent first (descending)
+      newPosts.sort((a, b) => (b.date - a.date));
+      return newPosts;
+    })
+
+    const promises = [];
+
+    promises.push(
+      setDoc(
+        doc(firebaseDb, "Users", posterUserId, "Posts", postId),
+        post,
+        { merge: true }
+      )
+    );
+    promises.push(
+      setDoc(doc(firebaseDb, "WhoPosts", postId), {
+        uid: posterUserId,
+      })
+    );
+
+    return Promise.all(promises);
+  }
+
+  // Run once (fetch data)
+  useEffect(() => {
+    // Async IIFE
+    (async () => {
+      onAuthStateChanged(firebaseAuth, async (newUser) => {
+        if (newUser) {
+          const postsRef = collection(firebaseDb, "Users", `${newUser.uid}`, "Posts");
+          const postsData = await getDocs(query(
+            postsRef,
+            orderBy("date"),
+            limit(10)
+          ));
+          const posts = postsData.docs.map(doc => ({
+            id: doc.id,
+            ...doc.data(),
+            date: doc.data().date.toDate() // convert Firebase Timestamp to JavaScript Date
+          }))
+
+          // Sort by date, most recent first (descending)
+          posts.sort((a, b) => (b.date - a.date));
+
+          setPosts(posts);
+        }
+      })
+    })();
+  }, []);
 
   return (
-    <div>
+    <div className="flex flex-col items-center">
       { user
         ? (
           <div>
+            <h1>Account Settings</h1>
             <div>
-              <h1>Account Settings</h1>
-
-              <input
-                type="text"
-                name="display_name"
-                value={inputDisplayName}
-                onChange={ e => setInputDisplayName(e.target.value) }
-              />
-              <button
-                onClick={async () => {
-                  await setDoc(doc(firebaseDb, "Users", `${user.uid}`), {
-                    displayName: inputDisplayName
-                  }, { merge: true })
-                }}
-              >
-                Set Username
-              </button>
+              <label htmlFor="display_name">Display Name</label>
+              <div className="flex justify-between">
+                <input
+                  type="text"
+                  id="display_name"
+                  value={inputDisplayName}
+                  onChange={ e => setInputDisplayName(e.target.value) }
+                />
+                <button
+                  className="ml-2"
+                  onClick={async () => {
+                    await setDoc(doc(firebaseDb, "Users", `${user.uid}`), {
+                      displayName: inputDisplayName
+                    }, { merge: true })
+                  }}
+                >
+                  Set
+                </button>
+              </div>
             </div>
 
-            <div className="flex flex-col">
-              <label htmlFor="post_title">Title</label>
-              <input
-                type="text"
-                name="post_title"
-                value={inputPostTitle}
-                onChange={ e => setInputPostTitle(e.target.value) }
-              />
+            <div className="flex flex-col gap-y-1 mt-4">
+              <h1>Create New Post</h1>
 
-              <label htmlFor="post_info">Info</label>
-              <input
-                type="text"
-                name="post_info"
-                value={inputPostInfo}
-                onChange={ e => setInputPostInfo(e.target.value) }
-              />
+              <div>
+                <label className="text-nowrap" htmlFor="post_title">Title</label>
+                <input
+                  type="text"
+                  id="post_title"
+                  value={inputPostTitle}
+                  onChange={ e => setInputPostTitle(e.target.value) }
+                />
+              </div>
 
-              <label htmlFor="post_content_uri">Content URI</label>
-              <input
-                type="URL"
-                name="post_content_uri"
-                value={inputPostContentURI}
-                onChange={ e => setInputPostContentURI(e.target.value) }
-              />
+              <div>
+                <label className="text-nowrap" htmlFor="post_info">Info</label>
+                <input
+                  type="text"
+                  id="post_info"
+                  value={inputPostInfo}
+                  onChange={ e => setInputPostInfo(e.target.value) }
+                />
+              </div>
 
-              <label htmlFor="post_thumbnail_uri">Thumbnail URI</label>
-              <input
-                type="URL"
-                name="post_thumbnail_uri"
-                value={inputPostThumbnailURI}
-                onChange={ e => setInputPostThumbnailURI(e.target.value) }
-              />
+              <div>
+                <label className="text-nowrap" htmlFor="post_content_uri">Content URI</label>
+                <input
+                  type="URL"
+                  id="post_content_uri"
+                  value={inputPostContentURI}
+                  onChange={ e => setInputPostContentURI(e.target.value) }
+                />
+              </div>
+
+              <div>
+                <label className="text-nowrap" htmlFor="post_thumbnail_uri">Thumbnail URI</label>
+                <input
+                  type="URL"
+                  id="post_thumbnail_uri"
+                  value={inputPostThumbnailURI}
+                  onChange={ e => setInputPostThumbnailURI(e.target.value) }
+                />
+              </div>
 
               <button
-                onClick={async () => {
-                  // TODO: Get unique post id
-                  const postId = "0";
-                  // many-to-one relationship between post ids and user ids
-                  // I want to be able to have a post ID and lookup the user id that posted
-                  // the post, that way I can actually get to where I need to go in the
-                  // database just form a post ID.
-                  // WhoPosts (collection):
-                  // `-- POST_ID (document)
-                  //     `-- USER_ID (field)
-                  // The above is BAD because it requires an entire new document for every
-                  // new post.
-                  // So, let's try inverting the data into keys and using a boolean value
-                  // (recommended by IBM for many-to-one relationships)
-                  // WhoPosts (collection):
-                  // `-- USER_ID (document)
-                  //     |-- POST_ID (field)
-                  //     `-- ...
-                  // Er, that doesn't work either. This would work if we wanted to be able
-                  // to efficiently query whether any given user did post a given post, but
-                  // this doesn't allow us to read back which user did post a given post.
-                  // From what I can tell, we kind of just have to use the first one.
-                  await setDoc(doc(firebaseDb, "Users", `${user.uid}`, "Posts", postId), {
-                    title: inputPostTitle,
-                    info: inputPostInfo,
-                    date: new Date(),
-                    type: "video",
-                    contentURI: inputPostContentURI,
-                    thumbnailURI: inputPostThumbnailURI,
-                  }, { merge: true });
-                  await setDoc(doc(firebaseDb, "WhoPosts", postId), {
-                    uid: user.uid,
-                  })
-                }}
+                className="mt-1"
+                onClick={
+                  e => postPost(
+                    user.uid,
+                    inputPostTitle,
+                    inputPostInfo,
+                    inputPostContentURI,
+                    inputPostThumbnailURI
+                  )
+                }
               >
                 Post
               </button>
-
-              <button
-                onClick={async () => {
-                  // TODO: Get unique post id (from post we are editing)
-                  const postId = "0";
-                  await deleteDoc(doc(firebaseDb, "Users", `${user.uid}`, "Posts", postId));
-                  await deleteDoc(doc(firebaseDb, "WhoPosts", postId));
-                }}
-              >
-                Delete Post
-              </button>
-
             </div>
 
-            <button className="text-red-600 pt-5 w-full" onClick={signOut}>Sign Out</button>
+            <div>
+              <h1>Your Posts</h1>
+
+              <div className="flex flex-col mt-2">
+                {
+                  posts.map(post => (
+                    <div
+                      className="flex justify-between items-center p-2 border border-slate-700 rounded"
+                      key={post.id}
+                    >
+                      <div className="flex justify-between gap-x-6 w-full overflow-hidden">
+                        <span className="truncate">{post.title}</span>
+                        <span className="overflow-clip">{post.date.toDateString()}</span>
+                      </div>
+                      <div className="flex ml-2">
+                        <button
+                          className="py-1 px-3"
+                          onClick={async () => {
+                            if (!confirm(`Really delete post titled "${post.title}?"`)) return;
+
+                            setPosts(oldPosts => {
+                              // Remove deleted post
+                              const newPosts = oldPosts.filter(candidate => candidate.id !== post.id);
+                              return newPosts;
+                            });
+
+                            let promises = [];
+                            promises.push(
+                              deleteDoc(doc(firebaseDb, "Users", `${user.uid}`, "Posts", post.id))
+                            );
+                            promises.push(
+                              deleteDoc(doc(firebaseDb, "WhoPosts", post.id))
+                            );
+                            await Promise.all(promises);
+                          }}
+                        >
+                          &#x1F5D1;
+                        </button>
+
+                        <a target="_blank" href={`${HOSTNAME}/see?p=${post.id}`}>
+                          <button className="py-1 px-3">&#9658;</button>
+                        </a>
+                      </div>
+                    </div>
+                  ))
+                }
+              </div>
+            </div>
+
+            <button className="text-red-600 my-4 w-full" onClick={signOut}>Sign Out</button>
           </div>
         )
         : (<>
