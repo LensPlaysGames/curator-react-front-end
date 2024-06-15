e47a46aa28cdd88751a55892e8d488d1289d11af
Lens_r <lensplaysgames@gmail.com>
2024-06-15 10:56:03 -0700

    [Analytics] Record `sign_up` event

--- src/components/SignInButton.tsx
+++ src/components/SignInButton.tsx
@@ -1,6 +1,12 @@
 "use client";
 
 import {
+  logEvent
+} from "firebase/analytics"
+import {
+  firebaseAnalytics
+} from "@/libs/firebase/config"
+import {
   signInPopupGoogle,
   type User
 } from "@/libs/firebase/auth";
@@ -9,12 +15,13 @@ export function SignInButton({ callback }: { callback: (user: User) => void }) {
   async function signIn() {
     const result = await signInPopupGoogle();
     callback(result);
+    logEvent(firebaseAnalytics, "sign_up", {
+      method: "Google"
+    })
   }
 
   return (
-    <button
-      onClick={signIn}
-    >
+    <button onClick={signIn}>
       Sign In
     </button>
   )
