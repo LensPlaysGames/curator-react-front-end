dbdf5f54a40282b41c259eaaaa9eda6d5a6524e5
Lens_r <lensplaysgames@gmail.com>
2024-06-15 21:49:02 -0700

    Implement RSS API

---
+++ src/app/api/rss/postsBy/[userId]/route.ts
@@ -0,0 +1,41 @@
+import RSS from "rss";
+import { HOSTNAME } from "@/constants";
+import { posts as fetchPosts } from "@/libs/api";
+
+export async function GET(request: Request, { params }: { params: { userId: string }}) {
+  const posterUserId = params.userId;
+
+  const data = await fetchPosts(posterUserId);
+
+  const rss = await generateRssFeed(data.posts, posterUserId);
+
+  return new Response(
+    rss,
+    {
+      headers: {
+        "Content-Type": "type/xml",
+      }
+    });
+}
+
+async function generateRssFeed(posts: Array<any>, posterUserId: string) {
+  const config = {
+    title: "", // TODO: Add display name of poster, among other things
+    description: "",
+    site_url: `${HOSTNAME}/u/${posterUserId}/`,
+    feed_url: `${HOSTNAME}/api/rss/postsBy/${posterUserId}/`,
+    pubDate: new Date(),
+  };
+  const rss = new RSS(config);
+
+  posts.map(post => {
+    rss.item({
+      title: post.title,
+      description: post.info,
+      url: `${HOSTNAME}/see/${post.id}/?u=${posterUserId}`,
+      date: post.date,
+    });
+  });
+
+  return rss.xml({ indent: true });
+}
