e78b1d8af61e569b547a17a298d4e5ae9e320972
Lens_r <lensplaysgames@gmail.com>
2024-06-18 12:42:39 -0700

    Be Selfish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import ClientHome from "./clientHome";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -6,8 +7,18 @@ export default function Home() {
       <ClientHome />
       <div className="panel">
         <h1>Featured Channels</h1>
+        <Link
+          className="border border-gray-500 rounded hover:text-cyan-500"
+          target="_blank"
+          href="/u/wc7tTL1uiAUOjl76zjH44uvAyTW2/"
+        >
+          <div className="flex items-center py-1 px-5 gap-x-5 text-lg">
+            <span className="text-3xl">&#x25b6;</span>
+            <span>lens_r</span>
+          </div>
+        </Link>
         <p>It&apos;s a bit quiet right now...</p>
-        <p>Make a few posts, contact the owner, and maybe you could show up here.</p>
+        <p className="font-bold">Make a few posts, contact the owner, and maybe you could show up here.</p>
       </div>
     </div>
   );
