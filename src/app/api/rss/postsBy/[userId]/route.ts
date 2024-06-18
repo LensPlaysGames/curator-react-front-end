82d8ed05ef8d6b1607b08baac521f59c5eee7899
Lens_r <lensplaysgames@gmail.com>
2024-06-18 12:41:14 -0700

    [RSS] Title and description fields now filled out somewhat reasonably

--- src/app/api/rss/postsBy/[userId]/route.ts
+++ src/app/api/rss/postsBy/[userId]/route.ts
@@ -1,13 +1,14 @@
 import RSS from "rss";
 import { HOSTNAME } from "@/constants";
-import { posts as fetchPosts } from "@/libs/api";
+import { userData as fetchUserData, posts as fetchPosts } from "@/libs/api";
 
 export async function GET(request: Request, { params }: { params: { userId: string }}) {
-  const posterUserId = params.userId;
+  const userId = params.userId;
 
-  const data = await fetchPosts(posterUserId);
+  const userData = await fetchUserData(userId);
+  const postsData = await fetchPosts(userId);
 
-  const rss = await generateRssFeed(data.posts, posterUserId);
+  const rss = await generateRssFeed(postsData.posts, userData, userId);
 
   return new Response(
     rss,
@@ -18,12 +19,12 @@ export async function GET(request: Request, { params }: { params: { userId: stri
     });
 }
 
-async function generateRssFeed(posts: Array<any>, posterUserId: string) {
+async function generateRssFeed(posts: Array<any>, userData, userId: string) {
   const config = {
-    title: "", // TODO: Add display name of poster, among other things
-    description: "",
-    site_url: `${HOSTNAME}/u/${posterUserId}/`,
-    feed_url: `${HOSTNAME}/api/rss/postsBy/${posterUserId}/`,
+    title: `${userData.displayName.length === 0 ? "Unnamed User" : userData.displayName} | Curator, vsp.`,
+    description: `${userData.displayName}'s channel on Curator, a Video Sharing Platform.`,
+    site_url: `${HOSTNAME}/u/${userId}/`,
+    feed_url: `${HOSTNAME}/api/rss/postsBy/${userId}/`,
     pubDate: new Date(),
   };
   const rss = new RSS(config);
@@ -32,7 +33,7 @@ async function generateRssFeed(posts: Array<any>, posterUserId: string) {
     rss.item({
       title: post.title,
       description: post.info,
-      url: `${HOSTNAME}/see/${post.id}/?u=${posterUserId}`,
+      url: `${HOSTNAME}/see/${post.id}/?u=${userId}`,
       date: post.date,
     });
   });
